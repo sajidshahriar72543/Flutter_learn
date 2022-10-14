f3fffbbf878f786e229a256fef078275f75f2eeb
sajidshahriar72543 <sazidshahriar39@gmail.com>
2022-10-14 14:02:58 +0600

    Introduced Appbar
    - Also learnt basics of adding icons to Appbar

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,24 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Test"),
-            RaisedButton(onPressed: (){})
-          ],
-        )
-      )
-    );
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("My App"),
+              centerTitle: true,
+              leading: Icon(Icons.menu),
+              actions: [
+                IconButton(icon: Icon(Icons.search), onPressed: () {}),
+              ],
+            ),
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Text("Test"), RaisedButton(onPressed: () {})],
+            )));
   }
 }
