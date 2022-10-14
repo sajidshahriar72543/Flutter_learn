c2638595a71333e21003fcc55d9db431ebfd2472
sajidshahriar72543 <sazidshahriar39@gmail.com>
2022-10-14 14:18:00 +0600

    Introduce Safe area
    Actually what safe area does is ignores the status bar of the app as a part of the app screen so that any element
    in the app screen gets rendered below the status bar. This is done by setting the top margin of the app screen

--- lib/main.dart
+++ lib/main.dart
@@ -9,30 +9,33 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text("My App"),
-              centerTitle: true,
-              leading: Icon(Icons.menu),
-              actions: [
-                IconButton(icon: Icon(Icons.search), onPressed: () {}),
-              ],
-            ),
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Test",
-                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                  ),
-                  RaisedButton(
-                    onPressed: () {},
-                    child: Text("Click Me"),
-                  ),
-                  ElevatedButton(onPressed: () {}, child: Text("Click Me")),
+        home: SafeArea(
+          child: Scaffold(
+              appBar: AppBar(
+                title: Text("My App"),
+                centerTitle: true,
+                leading: Icon(Icons.menu),
+                actions: [
+                  IconButton(icon: Icon(Icons.search), onPressed: () {}),
                 ],
               ),
-            )));
+              body: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Test",
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                    ),
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text("Click Me"),
+                    ),
+                    ElevatedButton(onPressed: () {}, child: Text("Click Me")),
+                  ],
+                ),
+              )),
+        ));
   }
 }
